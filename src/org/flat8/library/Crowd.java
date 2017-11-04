bf0ce3862d435819c65046fedf051ae9c26f2f9f
sandromax <sandrondk@gmail.com>
2017-11-04 23:14:19 +0200

    Creating Crowd and TextFileReader in library package

---
+++ src/org/flat8/library/Crowd.java
@@ -0,0 +1,10 @@
+package org.flat8.library;
+
+/**
+ * Class can create neaded amount of people
+ *
+ * @author  Oleksandr Maksymenko
+ * @version 0.0.1
+ */
+public class Crowd {
+}
---
+++ src/org/flat8/library/TextFileReader.java
@@ -0,0 +1,11 @@
+package org.flat8.library;
+
+/**
+ * Class read different text files.
+ * Can read file with next extensions: txt, doc, docx, dat, xls, html
+ *
+ * @author  Oleksandr Maksymenko
+ * @version 0.0.1
+ */
+public class TextFileReader {
+}
